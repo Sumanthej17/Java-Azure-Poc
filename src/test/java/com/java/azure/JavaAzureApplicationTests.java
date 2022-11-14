5c1e7a4f5ccd7afe4c71ebb92b8a4366c7b01b7a
Suman Y <suman.y@tavant.com>
2022-11-14 21:36:32 +0530

    updated test package

--- src/test/java/com/java/azure/JavaAzureApplicationTests.java
+++ src/test/java/com/java/azure/JavaAzureApplicationTests.java
@@ -1,10 +1,10 @@
-package com.poc.demo;
+package com.java.azure;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JavaAzureSampleApplicationTests {
+class JavaAzureApplicationTests {
 
 	@Test
 	void contextLoads() {
